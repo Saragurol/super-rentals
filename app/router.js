9934c02e972e384697087187ab2ac0bbeb5be98b
Linda Saraguro <lindasaraguro@yahoo.com>
2019-12-06 13:49:19 -0500

    created a contact route and template

--- app/router.js
+++ app/router.js
@@ -9,4 +9,6 @@ export default class Router extends EmberRouter {
 
 Router.map(function() {
   this.route('about');
+  this.route('contact', {path: '/getting-in-touch'});
+  // note: explicitly specified a path for the contact route
 });
