21ca0c0a2db952e3c944b6e285042061c031143c
Linda Saraguro <lindasaraguro@yahoo.com>
2019-12-07 17:53:53 -0500

    feat: added JSON data

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,21 +1,29 @@
 import Route from '@ember/routing/route'
 
+const COMMUNITY_CATEGORIES = [
+    'Condo',
+    'Townhouse',
+    'Apartment'
+  ];
+
 export default class IndexRoute extends Route {
     async model(){
         // model hook/method fetches any data for the route
-        return {
-            title:'Grand Old Mansion',
-            owner: 'Veruca Salt',
-            city: 'San Francisco',
-            location: {
-                lat: 37.7749,
-                lng: -122.4194,
-            },
-            category: 'Estate',
-            type: 'Standalone',
-            bedrooms: 15,
-            image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
-            description: 'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.'
-        }
+        let response = await fetch('/api/rentals.json');
+        // the Browser's fetch API requests the JSON data from our server's API at "public/api/rentals.json"
+        let {data} = await response.json();
+
+        return data.map(model => {
+            let { attributes } = model;
+            let type;
+
+            if(COMMUNITY_CATEGORIES.includes(attributes.category)){
+                type = "Community";
+            }else{
+                type="Standalone";
+            }
+
+            return { type, ...attributes }
+        })
     }
 }
